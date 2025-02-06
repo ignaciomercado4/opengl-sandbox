b0a6a2081f7b3275677db1dc8335738e399020b7
Ignacio Mercado <igmercado46@gmail.com>
2025-02-06 17:25:28 -0300

    triangle done

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Shader.hpp"
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
@@ -45,13 +46,42 @@ int main()
     {
         std::cout << "Error initializing GLAD" << std::endl;
         return -1;
-    } 
+    }
 
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // VERTS
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    // 1. gen VAO and VBO
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // 2. bind VAO and VBO
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // 3. configure and enable 'em    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    Shader ourShader("src/shaders/vertexShader.vert", "src/shaders/fragmentShader.frag");
 
     // MAIN LOOP
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.3f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
