2c6e998d74f6cfd16e0352a62b6da97cfb0ae684
Ignacio Mercado <igmercado46@gmail.com>
2025-02-06 22:37:33 -0300

    WASD movement added

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,15 @@
 #include <gtc/type_ptr.hpp>
 #include "Shader.hpp"
 
-
+// DEC&DEF
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+
+void processInput(GLFWwindow *window);
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 int main()
 {
@@ -58,11 +64,17 @@ int main()
     // LOCAL SPACE VERT COORDS
     float vertices[] = {
         // coord                // color
-        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,
-        0.0f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f};    
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f};
 
-    /*VAO AND VBO THING*/
+    // TRIANGLE WORLD SPACE POSITIONS
+    std::vector<glm::vec3> trianglePositions = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(1.5f, 0.0f, 0.0f),
+        glm::vec3(-1.5f, 0.0f, 0.0f)};
+
+    /*VAO AND VBO THING STEPS*/
     // 1. gen VAO and VBO
     unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
@@ -72,15 +84,14 @@ int main()
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-    // 3. configure and enable 'em    
+    // 3. configure and enable 'em
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // position 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    // position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
-    // color 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    // color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
     /*-----------------------------------------------------------------------------------*/
 
     Shader ourShader("src/shaders/vertexShader.vert", "src/shaders/fragmentShader.frag");
@@ -89,22 +100,23 @@ int main()
     // MAIN LOOP
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
         glClearColor(0.1f, 0.0f, 0.0f, 1.0f);
         glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         ourShader.use();
 
         // GOING 3D
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(model, glm::radians(80.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-
 
         int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -112,12 +124,19 @@ int main()
         int viewLoc = glGetUniformLocation(ourShader.ID, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-
         int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        for (unsigned int i = 0; i < trianglePositions.size(); i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, trianglePositions[i]);
+            model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+            ourShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -127,4 +146,25 @@ int main()
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    // MISC
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    { 
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    // MOVEMENT
+    const float cameraSpeed = 0.05f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
